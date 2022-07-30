1bf196b836bc31bf900112c93ea2568ff017fae0
jonasalberton <jonasceolin@gmail.com>
2022-07-30 08:19:28 -0300

    kubernets changes

--- micro-services/node-app/src/server.js
+++ micro-services/node-app/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var os = require("os");
+var hostname = os.hostname();
 
 const { userRouter } = require("./routes/user-router");
 const { transactionRouter } = require("./routes/transaction-router");
@@ -15,8 +17,9 @@ server.use("/api/user", userRouter);
 server.use("/api/transaction", transactionRouter);
 
 async function main() {
+  console.log('hostm', hostname);
   await mongoose.connect(DB);
-  server.listen(PORT, () => console.log("Server running!"));
+  server.listen(PORT, () => console.log("Server running!", hostname));
 }
 
 main().catch((err) => console.log(err));
